79254e4e1757e242385a9bd1453b9667e59e6f11
nguyenchithanh113 <55727999+nguyenchithanh113@users.noreply.github.com>
2023-07-07 17:52:38 +0700

    add spawner

---
+++ Assets/_Project/Scripts/System/SpawnerSystem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using _Project.Scripts.Component;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Scenes;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
+
+public partial struct SpawnerSystem : ISystem, ISystemStartStop
+{
+    private Random _random;
+    private SpawnerData _spawnerData;
+
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<SpawnerData>();
+        state.RequireForUpdate<EndInitializationEntityCommandBufferSystem.Singleton>();
+        _random = new Random(2);
+        
+    }
+    
+    
+
+    public void OnDestroy(ref SystemState state)
+    {
+
+    }
+
+    public void OnStartRunning(ref SystemState state)
+    {
+        _spawnerData = SystemAPI.GetSingleton<SpawnerData>();
+        EntityCommandBuffer commandBuffer = SystemAPI.GetSingleton<EndInitializationEntityCommandBufferSystem.Singleton>()
+            .CreateCommandBuffer(state.WorldUnmanaged);
+
+        
+        for (int i = 0; i < 1000; i++)
+        {
+            Entity entity = commandBuffer.Instantiate(_spawnerData.entityPrefab);
+            commandBuffer.SetComponent(entity, new MoveSpeedComponent()
+            {
+                speed = _random.NextFloat(5,20)
+            });
+        }
+    }
+
+    public void OnStopRunning(ref SystemState state)
+    {
+        
+    }
+}
